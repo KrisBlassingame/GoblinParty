567f4b151b26070e004dfac2773626bd112b216a
Kris Blassingame <krisblassingame@gmail.com>
2019-02-20 19:02:39 -0500

    Completed connection to card database

--- GoblinParty/Program.cs
+++ GoblinParty/Program.cs
@@ -20,7 +20,42 @@ namespace GoblinParty
 
             DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
 
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    Console.WriteLine("Connection Error");
+                    Console.ReadLine();
+                    return;
+                }
 
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+
+                if (command == null)
+                {
+                    Console.WriteLine("Command Error");
+                    Console.ReadLine();
+                    return;
+                }
+
+                command.Connection = connection;
+
+                command.CommandText = "Select * From Cards";
+
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($"{dataReader["CardId"]} " + $"{dataReader["Name"]}");
+                    }
+                }
+
+                Console.ReadLine();
+            }
 
 
 

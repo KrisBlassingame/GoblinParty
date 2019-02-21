d81a332120aac208b65ff112495556272a6c5c68
Kris Blassingame <krisblassingame@gmail.com>
2019-02-20 19:37:22 -0500

    created database class, deleted DB code from Program and added to DB class

--- GoblinParty/Program.cs
+++ GoblinParty/Program.cs
@@ -12,50 +12,14 @@ namespace GoblinParty
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            string provider = ConfigurationManager.AppSettings["provider"];
+            CardDatabase cardDB = new CardDatabase();
 
-            string connectionString = ConfigurationManager.AppSettings["connectionString"];
 
-            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
-
-            using (DbConnection connection = factory.CreateConnection())
-            {
-                if (connection == null)
-                {
-                    Console.WriteLine("Connection Error");
-                    Console.ReadLine();
-                    return;
-                }
-
-                connection.ConnectionString = connectionString;
-
-                connection.Open();
-
-                DbCommand command = factory.CreateCommand();
-
-                if (command == null)
-                {
-                    Console.WriteLine("Command Error");
-                    Console.ReadLine();
-                    return;
-                }
-
-                command.Connection = connection;
-
-                command.CommandText = "Select * From Cards";
-
-                using (DbDataReader dataReader = command.ExecuteReader())
-                {
-                    while (dataReader.Read())
-                    {
-                        Console.WriteLine($"{dataReader["CardId"]} " + $"{dataReader["Name"]}");
-                    }
-                }
-
-                Console.ReadLine();
-            }
+            cardDB.GetCardNamesFromDatabase();
 
 
 
